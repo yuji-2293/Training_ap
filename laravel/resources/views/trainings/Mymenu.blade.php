d9795dfb607439d1be5b312eb634745c88e9ced0
inoueyuji <sikogaku2593@gmail.com>
2023-10-16 20:28:31 +0900

    10月16日の進捗

--- laravel/resources/views/trainings/Mymenu.blade.php
+++ laravel/resources/views/trainings/Mymenu.blade.php
@@ -25,31 +25,38 @@
         <main class="grow m-10">
             <form action="{{route('part_store') }}" method="POST" class="max-w-7xl px-4 sm:px-6 mx-auto flex flex-col items-center">
                 <div class="flex flex-col items-center mt-12 mb-8  w-3/6">
-                    <label class="text-2xl font-bold text-center mb-5">登録するトレーニング部位</label>
-                    <small class="text-sm text-rose-600">※必須</small>
+                    <label class="text-2xl font-bold text-center mb-5 ">登録するトレーニング部位</label>
+                    <div class="mr-auto">
+                        <small class="text-sm ml-3"><カテゴリー></small>
+                        <small class="text-sm text-rose-600">※選択必須</small>
+                    </div>
+
                     <select type="text" class="form-control bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:border-sky-500 focus:ring-1 
-                     block p-2.5 w-full
+                     block p-2.5 w-full 
                     dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400
                     dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" name="part_id" id="part_id" required>
-                        <option disabled style='display:none;' @if (empty($My_menu_post->part_id))selected @endif >選択してください</option>
+                        <option disabled style='display:none;' class=""  @if (empty($My_menu_post->part_id))selected @endif >選択してください</option>
                         @foreach($parts as $part)
                             <option class="block" value="{{ $part->id }}"@if (isset($My_menu_post->part_id) && ($My_menu_post->part_id === $part_id)) selected @endif>{{ $part->name }}</option>
                         @endforeach
                     </select>
                 </div>
-                <div class="flex flex-col items-center w-full">
-                        <small class="text-sm text-rose-600">※入力欄</small>
-                            <div class="w-full max-w-3xl mx-auto flex ">
-                                    <input class="placeholder:italic placeholder:text-slate-400 block bg-white w-full 
-                                    border border-slate-300 rounded-md focus:outline-none focus:border-sky-500 focus:ring-1
-                                    py-4 pl-4 shadow-sm sm:text-sm mb-30  " placeholder="マイトレーニング" type="text" class="" name="name" id="name">
-                                    <input type="hidden" name="_token" value="{{ csrf_token() }}" />
+                <div class="flex flex-col items-center w-5/6">
+                    <div class="max-w-3xl block w-4/5">
+                        <small class="text-sm ml-3"><本文></small>
+                        <small class="text-sm text-rose-600">※入力必須</small>
+                    </div>
+                    <div class="w-full max-w-3xl  flex ">
+                            <input class="placeholder:italic placeholder:text-slate-400 block bg-white w-full 
+                            border border-slate-300 rounded-md focus:outline-none focus:border-sky-500 focus:ring-1
+                            py-4 pl-4 shadow-sm sm:text-sm mb-30  " placeholder="マイトレーニング" type="text" class="" name="name" id="name">
+                            <input type="hidden" name="_token" value="{{ csrf_token() }}" />
 
-                                    <div class="sm:text-sm ml-1">
-                                        <button type="submit"class="shadow-sm border border-slate-300 rounded-md py-4 w-20 md:hover:bg-sky-300 bg-sky-600 text-white  hover:bg-sky-300">登録する</button>
-                                    </div> 
-                                                
-                            </div>
+                            <div class="sm:text-sm ml-1">
+                                <button type="submit"class="shadow-sm border border-slate-300 rounded-md py-4 w-20 md:hover:bg-sky-300 bg-sky-600 text-white  hover:bg-sky-300">登録する</button>
+                            </div> 
+                                        
+                    </div>
                             <div class="">
                                 <a href="" type="button" class="shadow-sm border border-slate-300 block rounded-md p-3 mt-20 md:hover:bg-sky-300 bg-sky-600  hover:bg-sky-300 text-white ">>>登録したマイメニューを確認する</a>
 
@@ -64,16 +71,6 @@
                             @enderror
                 </div>
             </form>
-
-
-
-
- 
-
-
-
-
-
         </main>
 
         <footer class="bg-slate-500">
--- laravel/resources/views/trainings/index.blade.php
+++ laravel/resources/views/trainings/index.blade.php
@@ -14,6 +14,8 @@
     <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.8/index.global.min.js'></script>
     <!-- jquery CDN読み込み -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+
  </head>
  
  <!-- javascript -->
@@ -71,9 +73,29 @@
 </script>
 
 <!-- HTML -->
- <body class="flex flex-col min-h-[100vh] font-mono bg-slate-200">
-    <header class="bg-sky-600">
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 flex relative">
+<body class="flex flex-col min-h-[100vh] font-mono bg-slate-200">
+<header class="bg-sky-600">
+<div class="max-w-7xl mx-auto px-4 sm:px-6 flex relative">
+    <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Dropdown button <svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+  </svg>
+</button>
+
+<!-- Dropdown menu -->
+<div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+    <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+      <li>
+      <a href="{{ route('trainings.Mymenu') }}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">マイメニューの登録</a>
+      </li>
+      <li>       
+         <a href="{{ route('trainings.Mymenu') }}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">マイメニューの確認</a>
+      </li>
+      <li>
+        <a href="{{ route('trainings.create') }}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">ワークアウトの登録</a>
+      </li>
+
+    </ul>
+</div>
             <div class="py-4">
                 <a href="{{route('trainings.index')}}" class="text-white text-xl font-bold">筋トレ記録管理アプリ</a>
             </div>
@@ -106,6 +128,7 @@
             </div>
         </div>
 </footer>
+<script src="../path/to/flowbite/dist/flowbite.min.js"></script>
 
 </body>
 </html>
