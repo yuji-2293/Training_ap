911dcd8a9539804f7d2604e14c07b7ed0fbe14b4
inoueyuji <sikogaku2593@gmail.com>
2023-11-22 04:38:14 +0900

    11月21日進捗

--- laravel/resources/views/layouts/trainings/workouts_show.blade.php
+++ laravel/resources/views/layouts/trainings/workouts_show.blade.php
@@ -1,36 +1,63 @@
 <x-trainings.app>
-<main class="grow mx-auto max-w-7xl m-10">
 
-<ol class="relative border-s border-gray-600 dark:border-gray-700">
-    
-    @foreach($other_user_trainings as $index => $training)
-    @if($index < 3)
-    <li class="mb-10 ms-6 bg-white">
-        <h3 class=" flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">{{$training->user->name}}
-        <span class="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ms-3">{{$training->created_at->format('Y年m月d日') }}</span>
-        </h3>
-        <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+<main class="grow max-w-7xl m-10 w-full">
+
+<ol class="relative max-w-xl mx-auto border-s border-gray-600 dark:border-gray-700">
+        @foreach($other_user_trainings as $index => $training)
+        @if($index < 3)
+        <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3
+        mt-5 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
             <svg class="w-3.5 h-3.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                 <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
             </svg>
         </span>
-    <p>{{ $training->title }}</p>  
+     <li class="mb-10 ms-6 bg-white w-full max-w-6xl  border border-gray-200 rounded-lg shadow p-6 dark:bg-gray-800 dark:border-gray-700">
+                <div class="header text-center relative pb-2">
+                    <!-- ユーザー名 -->
+                    <h2 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">{{$training->user->name}}'sTraining!!
+                     <span class="absolute bottom-4 right-0 mt-4 bg-rose-500 text-white text-xs  font-medium  px-2.5 rounded dark:bg-blue-900 dark:text-white ms-3">{{$training->created_at->format('Y年m月d日') }}</span>
+                    </h2>
+                </div>
+            <div class="flex p-2 items-baseline ">
+                        <!-- トレーニング名 -->
+                        <div class="header mx-auto flex flex-col items-center">
+                            <label class="text-gray-600 my-1.5">トレーニング名</label>
+                            <p class="text-2xl font-bold text-gray-800">{{ $training->title }}</p>
+                        </div> 
+                       <div class="body ml-auto">
+                        @foreach($training->sets as $set)
+                        <div class="grid grid-cols-3 gap-4">
+                            <!-- 重量 -->
+                            <div class="flex flex-col items-center">
+                                <label class="text-gray-600 my-1.5">重量</label>
+                                <p class="text-lg font-semibold text-gray-800">{{ $set->weight }} kg</p>
+                            </div>
 
-    @foreach($training->sets as $set)
- 
-<p>{{ $set->set_id }}set</p>
-<p>{{ $set->weight }}kg</p>
-<p>{{ $set->rep }}回</p>
-@endforeach
-</li>
-@else
-@break
-    @endif
-    @endforeach
-    
-    </ol>
-{{$other_user_trainings->links()}}
+                            <!-- 回数 -->
+                            <div class="flex flex-col items-center">
+                                <label class="text-gray-600 my-1.5">回数</label>
+                                <p class="text-lg font-semibold text-gray-800">{{ $set->rep }} 回</p>
+                            </div>
 
+                            <!-- セット数 -->
+                            <div class="flex flex-col items-center">
+                                <label class="text-gray-600 my-1.5">セット数</label>
+                                <p class="text-lg font-semibold text-gray-800">{{ $set->set_id }} セット目</p>
+                            </div>
+                        </div> 
+
+                        @endforeach
+                </div>
+            </div>
+     </li>
+        @else
+        @break
+        @endif
+        @endforeach
+</ol>
+<div class="max-w-sm mx-auto">
+    {{$other_user_trainings->links()}}
+</div>
 </main>
 
 
---
+++ laravel/resources/views/vendor/pagination/tailwind.blade.php
@@ -0,0 +1,49 @@
+
+@if ($paginator->hasPages())
+    <nav role="navigation" aria-label="{{ __('Pagination Navigation') }}"
+     class="bg-white shadow-md rounded-md flex items-center justify-between py-3 px-3">
+        {{-- Previous Page Link --}}
+        @if ($paginator->onFirstPage())
+            <span class="disabled" aria-disabled="true" aria-label="{{ __('pagination.previous') }}">
+                <span class="text-gray-500">前へ</span>
+            </span>
+        @else
+            <a href="{{ $paginator->previousPageUrl() }}" rel="prev" class="text-blue-600 hover:text-blue-800" aria-label="{{ __('pagination.previous') }}">
+                &lsaquo;
+            </a>
+        @endif
+
+        {{-- Pagination Elements --}}
+        @foreach ($elements as $element)
+            {{-- "Three Dots" Separator --}}
+            @if (is_string($element))
+                <span class="text-gray-500">{{ $element }}</span>
+            @endif
+
+            {{-- Array Of Links --}}
+            @if (is_array($element))
+                @foreach ($element as $page => $url)
+                    @if ($page == $paginator->currentPage())
+                        <span class="text-rose-600">{{ $page }}</span>
+                    @else
+                        <a href="{{ $url }}" class="text-gray-500 hover:text-blue-600">
+                            {{ $page }}
+                        </a>
+                    @endif
+                @endforeach
+            @endif
+        @endforeach
+
+        {{-- Next Page Link --}}
+        @if ($paginator->hasMorePages())
+            <a href="{{ $paginator->nextPageUrl() }}" rel="next" class="hover:text-blue-800" aria-label="{{ __('pagination.next') }}">
+             <span class="text-gray-500">次へ</span>
+
+            </a>
+        @else
+            <span class="disabled" aria-disabled="true" aria-label="{{ __('pagination.next') }}">
+                <span class="text-gray-500">&rsaquo;</span>
+            </span>
+        @endif
+    </nav>
+@endif
