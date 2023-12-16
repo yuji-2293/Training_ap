8f62b4431b6b004201c3fe833834730e09837363
inoueyuji <sikogaku2593@gmail.com>
2023-12-16 19:08:05 +0900

    t

--- laravel/resources/js/calendar.js
+++ laravel/resources/js/calendar.js
@@ -41,7 +41,7 @@
                       document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 
                       $.ajax({
-                      url:  '/event',
+                      url:  'https://kukku999.xsrv.jp/event',
                       type: 'post',
                       data:eventData,
                       headers: {
