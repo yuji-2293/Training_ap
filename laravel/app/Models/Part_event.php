f0ea526d96b611c1ad77c1ac4fe93a06bac8671c
inoueyuji <sikogaku2593@gmail.com>
2023-12-16 15:08:21 +0900

    o

--- laravel/app/Models/Part_event.php
+++ laravel/app/Models/Part_event.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Part_event extends Model
+class part_event extends Model
 {
     protected $table = 'part_event';
     protected $fillable = ['title','start','category'];
