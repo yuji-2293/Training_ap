5ba6a86e152c8062960c3e9f1459a828085fe9b8
inoueyuji <sikogaku2593@gmail.com>
2023-12-17 23:55:36 +0900

    y

--- laravel/app/Http/Controllers/LikeController.php
+++ laravel/app/Http/Controllers/LikeController.php
@@ -18,55 +18,55 @@ class LikeController extends Controller
 {
     public function toggleLike(Training $training, Request $request)
 {
-    $user_id = Auth::user()->id;
-    $training_id = $request->training_id;
-    $existingLike = Like::where('user_id',$user_id)->where('training_id',$training_id);
+    // $user_id = Auth::user()->id;
+    // $training_id = $request->training_id;
+    // $existingLike = Like::where('user_id',$user_id)->where('training_id',$training_id);
 
-    if(!$existingLike){
-     $like = new Like;
-     $like->user_id = $user_id;
-     $like->training_id = $training_id;
-     $like->save(); 
-    } else {
-     Like::where('training_id',$training_id)->where('user_id',$user_id)->delete();
-    }
+    // if(!$existingLike){
+    //  $like = new Like;
+    //  $like->user_id = $user_id;
+    //  $like->training_id = $training_id;
+    //  $like->save(); 
+    // } else {
+    //  Like::where('training_id',$training_id)->where('user_id',$user_id)->delete();
+    // }
     
-    $likeCount = $training->likes()->count();
-    $param = [
-        'likeCount' => $likeCount,
-    ];
-    return response()->json($param);
+    // $likeCount = $training->likes()->count();
+    // $param = [
+    //     'likeCount' => $likeCount,
+    // ];
+    // return response()->json($param);
 
 
-//     $user = Auth::user();
+    $user = Auth::user();
 
-//     try {
-//         return DB::transaction(function () use ($training, $request, $user) {
-//             $existingLike = $training->likes()->where('user_id', $user->id)->first();
+    try {
+        return DB::transaction(function () use ($training, $request, $user) {
+            $existingLike = $training->likes()->where('user_id', $user->id)->first();
 
-//             if ($existingLike) {
-//                 // すでにいいねが存在する場合は削除
-//                 $existingLike->delete();
-//                 $likeCount = $training->likes()->count();
-//                 return response()->json(['isLiked' => false, 'likeCount' => $likeCount]);
-//             }
+            if ($existingLike) {
+                // すでにいいねが存在する場合は削除
+                $existingLike->delete();
+                $likeCount = $training->likes()->count();
+                return response()->json(['isLiked' => false, 'likeCount' => $likeCount]);
+            }
 
-//             // いいねが存在していない場合は新規作成
-//             $like = new Like();
-//             $like->user_id = $user->id;
-//             $like->training_id = $request->input('training_id');
-//             $like->save();
-//             $isLiked = true;
+            // いいねが存在していない場合は新規作成
+            $like = new Like();
+            $like->user_id = $user->id;
+            $like->training_id = $request->input('training_id');
+            $like->save();
+            $isLiked = true;
 
-//             // いいねの数を取得
-//             $likeCount = $training->likes()->count();
+            // いいねの数を取得
+            $likeCount = $training->likes()->count();
 
-//             return response()->json(['isLiked' => $isLiked, 'likeCount' => $likeCount, 'like' => $like]);
-//         });
-//     } catch (\Exception $e) {
-//         Log::error('Toggle Like error: ' . $e->getMessage(), ['user_id' => $user->id, 'training_id' => $request->input('training_id')]);
-//         return response()->json(['error' => 'An error occurred', 500]);
-//     }
+            return response()->json(['isLiked' => $isLiked, 'likeCount' => $likeCount, 'like' => $like]);
+        });
+    } catch (\Exception $e) {
+        Log::error('Toggle Like error: ' . $e->getMessage(), ['user_id' => $user->id, 'training_id' => $request->input('training_id')]);
+        return response()->json(['error' => 'An error occurred', 500]);
+    }
  }
     
 }
--- laravel/resources/views/layouts/trainings/workouts_show.blade.php
+++ laravel/resources/views/layouts/trainings/workouts_show.blade.php
@@ -3,8 +3,8 @@
 <main class="grow max-w-7xl m-10 w-full">
 
 <ol class="relative max-w-xl mx-auto border-s border-gray-600 dark:border-gray-700">
-        @foreach($other_user_trainings as $index => $training)
-        @if($index < 3)
+@foreach($other_user_trainings as $index => $training)
+@if($index < 3)
         <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3
         mt-5 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
             <svg class="w-3.5 h-3.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
@@ -25,7 +25,7 @@
                             <p class="text-2xl font-bold text-gray-800">{{ $training->title }}</p>
                         </div> 
                        <div class="body ml-auto">
-                         @foreach($training->sets as $set)
+@foreach($training->sets as $set)
                          <div class="grid grid-cols-3 gap-4">
                                 <!-- 重量 -->
                                 <div class="flex flex-col items-center">
@@ -48,25 +48,22 @@
 
                         </div>
                   </div>
-                @Auth
-                @if(!$training->isLikedBy(Auth::user()) )
+
                 <div class="like-container text-right my-2">
         
                 <button class="like-button" id="like-button-{{ $training->id }}" data-training-id="{{ $training->id }}">
                 <span class="like-status" data-training-id="{{ $training->id }}">
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
-</svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                    </svg>
                 </span>
                 </button>
-
-
-
-                <span class="like-count" id="like-count-{{ $training->id }}" data-training-id="{{ $training->id }}" >{{ $training->likes->count() }}
+                <span class="like-count" id="like-count-{{ $training->id }}" data-training-id="{{ $training->id }}" >
+                    {{ $training->likes->count() }}
                 </span>
                 </div>
-                @endif
-                @endAuth
+
+
             @endforeach
 
 </li>
