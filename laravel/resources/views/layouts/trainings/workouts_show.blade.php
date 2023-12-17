521d8ce505f48ac5a7a84268ee06d1bf5575874c
inoueyuji <sikogaku2593@gmail.com>
2023-12-18 00:10:00 +0900

    r

--- laravel/resources/views/layouts/trainings/workouts_show.blade.php
+++ laravel/resources/views/layouts/trainings/workouts_show.blade.php
@@ -111,15 +111,23 @@
                       withCredentials: true
                     },
                 success: function(data) {
-            // 成功したらいいね数とボタンのテキストを更新
+            // 成功したらいいね数とボタンのスタイルを更新
                 const $CountElement = $button.siblings('.like-count');
                 const $StatusElement = $button.find('.like-status');
                 // いいね状態が変わった場合はいいね数も更新
-                if(data.isLiked !==undefined) {
-                const newText = data.isLiked ? 'いいね済み': 'いいね解除';
-                $StatusElement.text(newText);
+                if (data.isLiked !== undefined) {
+                if (data.isLiked) {
+                    // いいね成功時のハートアイコン（ピンク色）に変更
+                    $StatusElement.html('<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-pink-500"><path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" /></svg>');
+                } else {
+                    // いいね解除時のハートアイコン（灰色）に変更
+                    $StatusElement.html('<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" /></svg>');
+                }
                 $CountElement.text(`${data.likeCount} いいね`);
-                console.log(data);
+                
+                // ボタンを再クリックできないように無効化
+                $button.prop('disabled', true);
+
                 location.reload();
 
                 }
@@ -138,8 +146,6 @@
             });
             });
             });
-
-
 </script>
 
 
