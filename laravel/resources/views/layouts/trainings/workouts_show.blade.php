fa4b9dcbee16aba0ebefc6d50ebe6f595ea42f10
inoueyuji <sikogaku2593@gmail.com>
2023-12-17 23:35:21 +0900

    u

--- laravel/resources/views/layouts/trainings/workouts_show.blade.php
+++ laravel/resources/views/layouts/trainings/workouts_show.blade.php
@@ -52,15 +52,16 @@
                 @if(!$training->isLikedBy(Auth::user()) )
                 <div class="like-container text-right my-2">
         
-                 <button class="like-button" id="like-button-{{ $training->id }}" data-training-id="{{ $training->id }}">
-                        <span class="like-status" data-training-id="{{ $training->id }}">
+                <button class="like-button" id="like-button-{{ $training->id }}" data-training-id="{{ $training->id }}">
+                <span class="like-status" data-training-id="{{ $training->id }}">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                         <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                        </svg>
+                </span>
+                </button>
 
-
-
-                        </span>
-                 </button>
-                 <span class="like-count" id="like-count-{{ $training->id }}" data-training-id="{{ $training->id }}" >{{ $training->likes->count() }}</span>
-
+                <span class="like-count" id="like-count-{{ $training->id }}" data-training-id="{{ $training->id }}" >{{ $training->likes->count() }}
+                </span>
                 </div>
                 @endif
                 @endAuth
