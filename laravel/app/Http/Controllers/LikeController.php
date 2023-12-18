d837ec4fdb5720646b43261a38d4063414dbe706
inoueyuji <sikogaku2593@gmail.com>
2023-12-18 17:39:43 +0900

    t

--- laravel/app/Http/Controllers/LikeController.php
+++ laravel/app/Http/Controllers/LikeController.php
@@ -16,58 +16,27 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 class LikeController extends Controller
 {
-    public function toggleLike(Training $training, Request $request)
+    public function toggleLike(Training $training, Like $lile,  Request $request)
 {
-    // $user_id = Auth::user()->id;
-    // $training_id = $request->training_id;
-    // $existingLike = Like::where('user_id',$user_id)->where('training_id',$training_id);
+   $user_id = Auth::user()->id;
+   $training_id = $request->training_id;
+   $existingLike = Like::where('user_id', $user_id)->where('training_id',$training_id)->first();
 
-    // if(!$existingLike){
-    //  $like = new Like;
-    //  $like->user_id = $user_id;
-    //  $like->training_id = $training_id;
-    //  $like->save(); 
-    // } else {
-    //  Like::where('training_id',$training_id)->where('user_id',$user_id)->delete();
-    // }
-    
-    // $likeCount = $training->likes()->count();
-    // $param = [
-    //     'likeCount' => $likeCount,
-    // ];
-    // return response()->json($param);
-
-
-    $user = Auth::user();
-
-    try {
-        return DB::transaction(function () use ($training, $request, $user) {
-            $existingLike = $training->likes()->lockForUpdate()->where('user_id', $user->id)->first();
-
-            if ($existingLike) {
-                // すでにいいねが存在する場合は削除
-                $existingLike->delete();
-                $likeCount = $training->likes()->count();
-                return response()->json(['isLiked' => false, 'likeCount' => $likeCount]);
-            } else {
+            if (!$existingLike) {
             // いいねが存在していない場合は新規作成
             $like = new Like();
-            $like->user_id = $user->id;
-            $like->training_id = $request->input('training_id');
+            $like->user_id = $user_id;
+            $like->training_id = $training_id;
             $like->save();
             $isLiked = true;
-            // いいねの数を取得
-            $likeCount = $training->likes()->count();
+            } else {
+                // すでにいいねが存在する場合は削除
+                Like::where('training_id', $training_id)->where('user_id',$user_id)->delete();
+                }
+                $likeCount = $training->likes()->count();
+                return response()->json(['isLiked' => false, 'likeCount' => $likeCount]);
+                return response()->json(['isLiked' => $isLiked, 'likeCount' => $likeCount, 'like' => $like]);
 
-            return response()->json(['isLiked' => $isLiked, 'likeCount' => $likeCount, 'like' => $like]);
-            }
-
-
-        });
-    } catch (\Exception $e) {
-        Log::error('Toggle Like error: ' . $e->getMessage(), ['user_id' => $user->id, 'training_id' => $request->input('training_id')]);
-        return response()->json(['error' => 'An error occurred', 500]);
-    }
  }
     
 }
