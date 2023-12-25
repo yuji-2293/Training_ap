21250444fc494aeae36ed732da0b03e66abf045a
inoueyuji <sikogaku2593@gmail.com>
2023-12-25 23:29:40 +0900

    e

--- laravel/resources/views/layouts/trainings/index.blade.php
+++ laravel/resources/views/layouts/trainings/index.blade.php
@@ -39,7 +39,7 @@ Toggle modal
 </button>
 
 <div id="popup-modal" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-    <div class="relative p-4 w-full max-w-md max-h-full mx-auto ">
+    <div class="relative p-4 w-full max-w-md max-h-full mx-auto my-auto">
         <div class="relative bg-sky-600 rounded-lg shadow dark:bg-gray-700">
             <!-- 閉じるボタン -->
             <button type="button" class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="popup-modal">
