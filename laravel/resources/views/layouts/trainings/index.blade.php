7940fa947f5afcd2be97177f571965a2fcb34b09
inoueyuji <sikogaku2593@gmail.com>
2023-12-25 23:12:03 +0900

    ww

--- laravel/resources/views/layouts/trainings/index.blade.php
+++ laravel/resources/views/layouts/trainings/index.blade.php
@@ -33,12 +33,7 @@
     </div>
 </div>
 
-
-<button data-modal-target="popup-modal" data-modal-toggle="popup-modal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
-Toggle modal
-</button>
-
-<div id="popup-modal" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+<div id="popup-modal" tabindex="-1" class="hidden mx-auto overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
     <div class="relative p-4 w-full max-w-md max-h-full">
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
             <!-- 閉じるボタン -->
@@ -48,7 +43,8 @@ Toggle modal
                 </svg>
             </button>
             <!-- モーダルの中身 -->
-            <div class="p-4 md:p-5 text-center mx-auto">
+            <div class="p-4 md:p-5 text-center">
+                <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">テキスト</h3>
                 <button data-modal-hide="popup-modal" type="button" class="text-white bg-rose-500 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center me-2">
                 remove modeに移行します
                 </button>
