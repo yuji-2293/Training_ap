8573c210391497ae362fc0cac37d577d841b75f9
inoueyuji <sikogaku2593@gmail.com>
2023-12-25 23:21:32 +0900

    o

--- laravel/resources/views/layouts/trainings/index.blade.php
+++ laravel/resources/views/layouts/trainings/index.blade.php
@@ -48,7 +48,7 @@ Toggle modal
                 </svg>
             </button>
             <!-- モーダルの中身 -->
-            <div class="p-4 md:p-5 text-center absolute top-5">
+            <div class="p-4 md:p-5 text-center">
                 <button data-modal-hide="popup-modal" type="button" class="text-white bg-rose-500 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center me-2">
                 登録削除モード
                 </button>
