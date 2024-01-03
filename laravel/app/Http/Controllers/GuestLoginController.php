ad358dc31d8780bf1458b6488f5091c7e858cc0b
inoueyuji <sikogaku2593@gmail.com>
2024-01-03 18:10:48 +0900

    r

--- laravel/app/Http/Controllers/GuestLoginController.php
+++ laravel/app/Http/Controllers/GuestLoginController.php
@@ -10,10 +10,10 @@ class GuestLoginController extends Controller
     public function store()
     {
      $email = 'guest@example.com';
-     $password = 'guestPassword';
+     $password = 'password';
      if (Auth::attempt(['email' => $email, 'password' => $password])) {
      //ログイン成功
-     return redirect()->intended('dashbord');
+     return redirect()->intended('dashboard');
     }
      //ログイン失敗
      return redirect()->route('login')->withErrors([
