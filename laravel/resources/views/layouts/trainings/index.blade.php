65b4eed4d8dfd25ee3b6960422b9dfd682190d95
inoueyuji <sikogaku2593@gmail.com>
2024-01-07 23:19:54 +0900

    s

--- laravel/resources/views/layouts/trainings/index.blade.php
+++ laravel/resources/views/layouts/trainings/index.blade.php
@@ -8,26 +8,25 @@
  <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
  <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.8/index.global.min.js'></script>
 <div class="flex justify-center items-center">
-    <div class="flex">
-            <!-- dashboard -->
-    <div id="dashboard" class="h-80  bg-white text-center m-8 rounded-md shadow">
-        <div id="categories" class="shadow bg-sky-100  m-2 mt-4 rounded-md" >
-            <div class="category p-2 rounded-md border border border-slate-300 md:hover:bg-sky-300 bg-rose-500 text-white  hover:bg-sky-300" data-category="胸">胸</div>
-            <div class="category p-2 rounded-md border border border-slate-300 md:hover:bg-sky-300 bg-rose-500 text-white  hover:bg-sky-300" data-category="背中">背中</div>
-            <div class="category p-2 rounded-md border border border-slate-300 md:hover:bg-sky-300 bg-rose-500 text-white  hover:bg-sky-300" data-category="足">足</div>
-            <div class="category p-2 rounded-md border border border-slate-300 md:hover:bg-sky-300 bg-rose-500 text-white  hover:bg-sky-300" data-category="腕or肩">腕or肩</div>
-            <div class="category p-2 rounded-md border border border-slate-300 md:hover:bg-sky-300 bg-rose-500 text-white  hover:bg-sky-300" data-category="その他">その他</div>
-        </div>
-        <div class="mt-6">
-            <label class="text-sm font-bold">
-                <input class="text-rose-500 focus:border-rose-500 focus:ring-pink-500" type="checkbox" id="deleteModeCheckbox">
-                削除
-            </label>
-        </div>
-    </div>
-                    <div class="arrow-container mx-5">
-                    <div class="arrow animate-blink text-rose-500">→</div> 
+                    <!-- dashboard -->
+            <div id="dashboard" class="h-80  bg-white text-center rounded-md shadow">
+                <div id="categories" class="shadow bg-sky-100  m-2 mt-4 rounded-md" >
+                    <div class="category p-2 rounded-md border border border-slate-300 md:hover:bg-sky-300 bg-rose-500 text-white  hover:bg-sky-300" data-category="胸">胸</div>
+                    <div class="category p-2 rounded-md border border border-slate-300 md:hover:bg-sky-300 bg-rose-500 text-white  hover:bg-sky-300" data-category="背中">背中</div>
+                    <div class="category p-2 rounded-md border border border-slate-300 md:hover:bg-sky-300 bg-rose-500 text-white  hover:bg-sky-300" data-category="足">足</div>
+                    <div class="category p-2 rounded-md border border border-slate-300 md:hover:bg-sky-300 bg-rose-500 text-white  hover:bg-sky-300" data-category="腕or肩">腕or肩</div>
+                    <div class="category p-2 rounded-md border border border-slate-300 md:hover:bg-sky-300 bg-rose-500 text-white  hover:bg-sky-300" data-category="その他">その他</div>
                 </div>
+                <div class="mt-6">
+                    <label class="text-sm font-bold">
+                        <input class="text-rose-500 focus:border-rose-500 focus:ring-pink-500" type="checkbox" id="deleteModeCheckbox">
+                        削除
+                    </label>
+                </div>
+            </div>
+            <div class="arrow-container mx-5 self-stretch flex items-center">
+            <div class="arrow animate-blink text-rose-500">→</div> 
+
     </div>
 
 
@@ -35,7 +34,7 @@
 
 <!-- calendarをブラウザに表示 -->
     <div id="app">
-        <div class="m-auto m-7 p-7 max-w-7xl">
+        <div class="max-w-7xl">
             <div id='calendar' class="bg-white p-7 rounded-lg shadow"></div>
         </div>
     </div>
