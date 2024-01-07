13b118d4ae7b1e4e277833f1e520dd814fb7e147
inoueyuji <sikogaku2593@gmail.com>
2024-01-07 22:16:42 +0900

    e

--- laravel/resources/views/layouts/trainings/index.blade.php
+++ laravel/resources/views/layouts/trainings/index.blade.php
@@ -11,16 +11,21 @@
     <!-- dashboard -->
     <div id="dashboard" class="w-1/12 h-80  bg-white text-center m-8 rounded-md shadow">
         <div id="categories" class="shadow w-2/3 bg-sky-100 mx-auto m-2 mt-4 rounded-md" >
-            <div class="category p-2 rounded-md border border border-slate-300 md:hover:bg-sky-300 bg-rose-500 text-white  hover:bg-sky-300" data-category="胸">
-            <span>胸</span>
-            <span class="arrow">→</span>
+            <div class="flex">
+                <div class="category p-2 rounded-md border border border-slate-300 md:hover:bg-sky-300 bg-rose-500 text-white  hover:bg-sky-300" data-category="胸">
+                 胸
+                </div>            
+            <span class="arrow text-rose-500">→</span> 
             </div>
+   
+
             
             <div class="category p-2 rounded-md border border border-slate-300 md:hover:bg-sky-300 bg-rose-500 text-white  hover:bg-sky-300" data-category="背中">背中</div>
             <div class="category p-2 rounded-md border border border-slate-300 md:hover:bg-sky-300 bg-rose-500 text-white  hover:bg-sky-300" data-category="足">足</div>
             <div class="category p-2 rounded-md border border border-slate-300 md:hover:bg-sky-300 bg-rose-500 text-white  hover:bg-sky-300" data-category="腕or肩">腕or肩</div>
             <div class="category p-2 rounded-md border border border-slate-300 md:hover:bg-sky-300 bg-rose-500 text-white  hover:bg-sky-300" data-category="その他">その他</div>
         </div>
+
         <div class="mt-6">
             <label class="text-sm font-bold">
                 <input class="text-rose-500 focus:border-rose-500 focus:ring-pink-500" type="checkbox" id="deleteModeCheckbox">
