47a2e3243a4d0e89228f34671cfe76819db563ef
inoueyuji <sikogaku2593@gmail.com>
2024-01-07 22:37:02 +0900

    r

--- laravel/resources/views/layouts/trainings/index.blade.php
+++ laravel/resources/views/layouts/trainings/index.blade.php
@@ -10,20 +10,18 @@
 <div class="flex">
     <!-- dashboard -->
     <div id="dashboard" class="w-1/12 h-80  bg-white text-center m-8 rounded-md shadow">
-        <div id="categories" class="shadow w-2/3 bg-sky-100 mx-auto m-2 mt-4 rounded-md" >
-                <div class="category p-2 rounded-md border border border-slate-300 md:hover:bg-sky-300 bg-rose-500 text-white  hover:bg-sky-300" data-category="胸">
-                 胸
-                </div>            
-                <div class="arrow text-rose-500">→</div> 
-            
-   
+            <div class="flex">
+                <div id="categories" class="shadow w-2/3 bg-sky-100 mx-auto m-2 mt-4 rounded-md" >
+                    <div class="category p-2 rounded-md border border border-slate-300 md:hover:bg-sky-300 bg-rose-500 text-white  hover:bg-sky-300" data-category="胸">胸</div>
+                    <div class="category p-2 rounded-md border border border-slate-300 md:hover:bg-sky-300 bg-rose-500 text-white  hover:bg-sky-300" data-category="背中">背中</div>
+                    <div class="category p-2 rounded-md border border border-slate-300 md:hover:bg-sky-300 bg-rose-500 text-white  hover:bg-sky-300" data-category="足">足</div>
+                    <div class="category p-2 rounded-md border border border-slate-300 md:hover:bg-sky-300 bg-rose-500 text-white  hover:bg-sky-300" data-category="腕or肩">腕or肩</div>
+                    <div class="category p-2 rounded-md border border border-slate-300 md:hover:bg-sky-300 bg-rose-500 text-white  hover:bg-sky-300" data-category="その他">その他</div>
+                </div>
 
-            
-            <div class="category p-2 rounded-md border border border-slate-300 md:hover:bg-sky-300 bg-rose-500 text-white  hover:bg-sky-300" data-category="背中">背中</div>
-            <div class="category p-2 rounded-md border border border-slate-300 md:hover:bg-sky-300 bg-rose-500 text-white  hover:bg-sky-300" data-category="足">足</div>
-            <div class="category p-2 rounded-md border border border-slate-300 md:hover:bg-sky-300 bg-rose-500 text-white  hover:bg-sky-300" data-category="腕or肩">腕or肩</div>
-            <div class="category p-2 rounded-md border border border-slate-300 md:hover:bg-sky-300 bg-rose-500 text-white  hover:bg-sky-300" data-category="その他">その他</div>
-        </div>
+                <div class="arrow text-rose-500">→</div> 
+            </div>
+
 
         <div class="mt-6">
             <label class="text-sm font-bold">
