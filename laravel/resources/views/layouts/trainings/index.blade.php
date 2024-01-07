826a3695ca66ddc06f92c8b6b78afec26fba9689
inoueyuji <sikogaku2593@gmail.com>
2024-01-08 00:22:04 +0900

    4

--- laravel/resources/views/layouts/trainings/index.blade.php
+++ laravel/resources/views/layouts/trainings/index.blade.php
@@ -29,7 +29,7 @@
             </div>
     </div>
 
-            <div class="arrow-container mt-12">
+            <div class="arrow-container mt-12 p-4">
                 <div class="arrow animate-blink text-rose-500">→</div> 
             </div>
         <!-- calendarをブラウザに表示 -->
