988dca5052d07e503a60297735cbaf88b33695b2
inoueyuji <sikogaku2593@gmail.com>
2024-01-07 23:46:09 +0900

    p

--- laravel/resources/views/layouts/trainings/index.blade.php
+++ laravel/resources/views/layouts/trainings/index.blade.php
@@ -25,7 +25,7 @@
                 </div>
             </div>
             <div class="arrow-container mx-5 flex flex-col justify-center items-center">
-            <span class="drag-drop-text text-xs text-gray-500 opacity-75">drag and drop!</span>
+            <span class="drag-drop-text text-xs text-gray-500 opacity-75">drag & drop!</span>
 
             <div class="arrow animate-blink text-rose-500">→</div> 
 
