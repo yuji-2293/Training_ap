145221ff9dfa3070d95aace46d002bf902ce254e
inoueyuji <sikogaku2593@gmail.com>
2024-01-15 21:45:34 +0900

    ree

--- laravel/resources/views/layouts/trainings/index.blade.php
+++ laravel/resources/views/layouts/trainings/index.blade.php
@@ -14,7 +14,7 @@
         </div>
             <!-- dashboard -->
             <div id="dashboard" class="h-full  bg-white text-center rounded-md shadow">
-                <div id="categories" class="flex flex-col items-center shadow bg-sky-100 m-2 mt-4 p-4 rounded-md" >
+                <div id="categories" class="shadow bg-sky-100 m-2 mt-4 p-4 rounded-md flex flex-col items-center" >
                     <div class="category mb-4 p-6 flex items-center justify-center w-12 h-12 rounded-full bg-rose-500 text-white text-base font-bold hover:bg-sky-300" data-category="胸">胸</div>
                     <div class="category mb-4 p-6 flex items-center justify-center w-12 h-12 rounded-full bg-rose-500 text-white text-base font-bold hover:bg-sky-300" data-category="背中">背中</div>
                     <div class="category mb-4 p-6 flex items-center justify-center w-12 h-12 rounded-full bg-rose-500 text-white text-base font-bold hover:bg-sky-300" data-category="足">足</div>
